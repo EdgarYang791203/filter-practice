f40c22344b11acffeb1ed58cd907a4bdd7035bd5
EdgarYang791203 <73307hank@gmail.com>
2018-07-02 21:46:39 +0800

    initMap

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var current = document.getElementsByClassName("active");
 var total = document.querySelector("#total");
 var url = "https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97&limit=268";
 var str;
+var showStr;
 var showInformation = document.querySelector("#showInformation");
 var selectorLocation = document.getElementById("selectorLocation");
+var showDetail = document.getElementById("showDetail");
 var free = document.getElementById("free");
 var allDay = document.getElementById("allDay");
 var arrA = [];
@@ -18,14 +20,15 @@ function render() {
         if (arrB[i] == undefined) {
             continue;
         }
-        str += `<div data-num="${i}" class="result">
+        str += `<div onclick="show(${i})" class="result">
                     <img src="${arrB[i].Picture1}" alt="">
                     <div class="content">
                         <h3>${arrB[i].Name}</h3>
-                        <span>${arrB[i].Ticketinfo}</span>
+                        <span>${ticketinfo(arrB[i].Ticketinfo)}</span>
                         <p>${arrB[i].Description}</p>
                         <h4>位置</h4>
                         <span>${arrB[i].Zone}</span>
+                        <img onclick="initMap(${arrB[i].Py},${arrB[i].Px})" id="mapBtn" src="https://goo.gl/w8NN6B">
                         <div class="information">
                             <i class="fas fa-map-marker-alt"></i>
                             <span>${arrB[i].Add}</span>
@@ -103,14 +106,15 @@ function xhrListener() {
 
     for (var count = 0; count < 10; count++) {
         str +=
-            `<div data-num="${i}" class="result">
+            `<div onclick="show(${count})" class="result">
                 <img src="${arrA[count].Picture1}" alt="">
                 <div class="content">
                     <h3>${arrA[count].Name}</h3>
-                    <span>${arrA[count].Ticketinfo}</span>
+                    <span>${ticketinfo(arrA[count].Ticketinfo)}</span>
                     <p>${arrA[count].Description}</p>
                     <h4>位置</h4>
                     <span>${arrA[count].Zone}</span>
+                    <img onclick="initMap(${arrA[count].Py},${arrA[count].Px})" id="mapBtn" src="https://goo.gl/w8NN6B">
                     <div class="information">
                         <i class="fas fa-map-marker-alt"></i>
                         <span>${arrA[count].Add}</span>
@@ -214,23 +218,86 @@ window.addEventListener("load", function () {
     });
 });
 
+function ticketinfo(x) {
+    if (x == "") {
+        return "請查詢官網是否收費";
+    } else {
+        return "免費參觀";
+    }
+}
+
+function show(x) {
+    if (event.target.id == "mapBtn") {
+        return;
+    }
+    // console.log(arrA[x]);
+    // console.log(arrB[x]);
+    showStr = "";
+    showInformation.classList.add("hidden");
+    showDetail.classList.remove("hidden");
+    document.getElementById("pages").classList.add("hidden");
+    if (arrB.length === 0) {
+        showStr =
+            `<button class="btn btn-outline-primary" style="margin-bottom:20px;" onclick="previous()">回上一頁</button>
+            <img src="${arrA[x].Picture1}" alt="">
+            <div class="content">
+                <h3>${arrA[x].Name}</h3>
+                <span>${ticketinfo(arrA[x].Ticketinfo)}</span>
+                <p>${arrA[x].Description}</p>
+                <h4>位置</h4>
+                <span>${arrA[x].Zone}</span>
+                <div class="information">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <span>${arrA[x].Add}</span>
+                    <i class="far fa-calendar-alt"></i>
+                    <span>${arrA[x].Opentime}</span>
+                </div>
+            </div>`;
+    } else {
+        showStr =
+            `<button class="btn btn-outline-primary" style="margin-bottom:20px;" onclick="previous()">回上一頁</button>
+            <img src="${arrB[x].Picture1}" alt="">
+            <div class="content">
+                <h3>${arrB[x].Name}</h3>
+                <span>${ticketinfo(arrA[x].Ticketinfo)}</span>
+                <p>${arrB[x].Description}</p>
+                <h4>位置</h4>
+                <span>${arrB[x].Zone}</span>
+                <div class="information">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <span>${arrB[x].Add}</span>
+                    <i class="far fa-calendar-alt"></i>
+                    <span>${arrB[x].Opentime}</span>
+                </div>
+            </div>`;
+    }
+    showDetail.innerHTML = showStr;
+}
+
+function previous() {
+    showInformation.classList.remove("hidden");
+    showDetail.classList.add("hidden");
+    document.getElementById("pages").classList.remove("hidden");
+}
+
 function changePage() {
     var active = document.querySelector(".active");
     showInformation.innerHTML = "";
     str = "";
     for (var i = active.dataset.num * 10; i < (active.dataset.num * 10) + 10; i++) {
-        if (arrA[i] == undefined || arrB[i] == undefined) {
-            return;
+        if (arrA[i] == undefined) {
+            continue;
         } else if (arrB.length === 0) {
             str +=
-                `<div data-num="${i}" class="result">
+                `<div onclick="show(${i})" class="result">
                 <img src="${arrA[i].Picture1}" alt="">
                 <div class="content">
                     <h3>${arrA[i].Name}</h3>
-                    <span>${arrA[i].Ticketinfo}</span>
+                    <span>${ticketinfo(arrA[i].Ticketinfo)}</span>
                     <p>${arrA[i].Description}</p>
                     <h4>位置</h4>
                     <span>${arrA[i].Zone}</span>
+                    <img onclick="initMap(${arrA[i].Py},${arrA[i].Px})" id="mapBtn" src="https://goo.gl/w8NN6B">
                     <div class="information">
                         <i class="fas fa-map-marker-alt"></i>
                         <span>${arrA[i].Add}</span>
@@ -239,17 +306,20 @@ function changePage() {
                     </div>
                 </div>
             </div>`;
-            showInformation.innerHTML = str;
         } else {
+            if (arrB[i] == undefined) {
+                continue;
+            }
             str +=
-                `<div data-num="${i}" class="result">
+                `<div onclick="show(${i})" class="result">
                 <img src="${arrB[i].Picture1}" alt="">
                 <div class="content">
                     <h3>${arrB[i].Name}</h3>
-                    <span>${arrB[i].Ticketinfo}</span>
+                    <span>${ticketinfo(arrB[i].Ticketinfo)}</span>
                     <p>${arrB[i].Description}</p>
                     <h4>位置</h4>
                     <span>${arrB[i].Zone}</span>
+                    <img onclick="initMap(${arrB[i].Py},${arrB[i].Px})" id="mapBtn" src="https://goo.gl/w8NN6B">
                     <div class="information">
                         <i class="fas fa-map-marker-alt"></i>
                         <span>${arrB[i].Add}</span>
@@ -258,7 +328,41 @@ function changePage() {
                     </div>
                 </div>
             </div>`;
-            showInformation.innerHTML = str;
         }
     }
+    showInformation.innerHTML = str;
+}
+
+$(window).ready(function () {
+    $(document).on("click", ".content", function (e) {
+        if (e.target.nodeName == "IMG") {
+            $("#mapBg").removeClass("hidden");
+        }
+    });
+    $("#mapBg").click(function (e) {
+        if (e.target.id == "mapBg") {
+            $("#mapBg").addClass("hidden");
+        }
+    });
+});
+
+function initMap(x, y) {
+    var map;
+
+    var mapCenter = {
+        lat: x,
+        lng: y
+    };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: mapCenter,
+        zoom: 16,
+        //styles: mapStyle,
+        draggable: true
+    });
+    var maker = new google.maps.Marker({
+        position: mapCenter,
+        map: map
+        //title: "",
+        //icon: "travel.png"
+    });
 }
